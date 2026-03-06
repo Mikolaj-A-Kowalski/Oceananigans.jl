2dc3e458e82040eb732cda4e1473445bc151bb16
M.A. Kowalski <mak60@cam.ac.uk>
2026-03-06 06:58:34 -0500

    refactor: use more manual property-based testing for fast float
    At the moment only unary case is set up.

--- test/test_fast_float.jl
+++ test/test_fast_float.jl
@@ -2,6 +2,9 @@ include("dependencies_for_runtests.jl")
 
 using Oceananigans.FastFloats: FastFloat
 import InteractiveUtils: methodswith
+import Supposition
+import Supposition.Data
+
 
 @testset "Conversion and Promotion" begin
     # Null promotion
@@ -25,93 +28,39 @@ import InteractiveUtils: methodswith
 
 end
 
+#################
+# Function tests
 
-function floating_point_test_inputs(::Type{FT}) where {FT<:Base.IEEEFloat}
-    return FT[
-        0.0,
-        -0.0,
-        1.0,
-        -1.0,
-        0.5,
-        -0.5,
-        1e-10,
-        -1e-10,
-        1e10,
-        -1e10,
-        # We don't want to enforce equivalent result at the very top of the range
-        # only few binades below the max value
-        floatmax(FT)/8,
-        -floatmax(FT)/8,
-    ]
+# Test assertions
+strictly_equal(x, y) = (x == y)
+
+
+# Generator constructor
+function make_generator(::Type{FT}, min, max) where {FT<:Base.IEEEFloat}
+    return Supposition.Data.Floats{FT}(;
+        minimum = min,
+        maximum = max,
+        infs = false,
+        nans = false,
+    )
+end
+function make_generator(::Type{IT}, min, max) where {IT<:Integer}
+    return Supposition.Data.Integers{IT}(min, max)
 end
 
 
-# We don't care about the inputs out of 'normal' domain
-# Hence we skip the test if a reference is NaN or Inf or it throws a DomainError
-# Other wise we check for equality with several ULP tolerance for FP outputs
-# exact equality for other
-function test_single_case(f, ref_inputs, test_inputs)
-    ref_output = try
-        f(ref_inputs...)
-    catch e
-        if isa(e, DomainError)
-            return
-        else
-            rethrow(e)
+@testset "FastFloat unary functions" begin
+    for (operator, input_range, test_assertion) in(
+        (:(sin), (-Inf, Inf), strictly_equal),
+        (:(asin), (-1.0, 1.0), strictly_equal)
+    )
+        @eval begin
+            Supposition.@check function $(Symbol("check_", operator))(
+                fp_number = make_generator(Float64, $input_range...),
+            )
+                ff = FastFloat(fp_number)
+                return $test_assertion($operator(ff), $operator(fp_number))
+            end
         end
     end
-
-    test_output = f(test_inputs...)
-
-    if isa(ref_output, Number) && (isnan(ref_output) || isinf(ref_output))
-        return
-    elseif isa(ref_output, AbstractFloat)
-        @test isapprox(test_output, ref_output; atol = 0, rtol = 10*eps(typeof(ref_output)))
-    else
-        @test test_output == ref_output
-    end
-end
-
-
-# Assumes all arguments of the function are FastFloats
-function test_single_fastfloat_function(::Type{FT}, m::Method) where {FT}
-    (_, argument_types...) = m.sig.parameters
-    func = eval(m.name)
-    n_args = length(argument_types)
-
-    ref_argument_types = fill(FT, n_args)
-    input_values_for_each_argument = fill(floating_point_test_inputs(FT), n_args)
-
-    for inputs in Iterators.product(input_values_for_each_argument...)
-        ref_inputs = map((t, v) -> t(v), ref_argument_types, inputs)
-        test_inputs = map((t, v) -> t(v), argument_types, inputs)
-
-        test_single_case(func, ref_inputs, test_inputs)
-    end
-end
-
-
-function has_only_fast_float_args(m::Method)
-    (_, arg_types...) = m.sig.parameters
-    return all(t -> t <: FastFloat, arg_types)
-end
-
-
-# Some of the operators need to be skipped and be tested by explicit tests
-const function_blacklist = Set((
-    :convert,
-    :promote,
-
-    # Broken due to other problems...
-    :flipsign,
-    :signbit,
-))
-
-
-@testset "Test function: $(m.name)" for m in [
-    m for m in methodswith(FastFloat, Base; supertypes = false) if
-    m.name ∉ function_blacklist && has_only_fast_float_args(m)
-]
-    test_single_fastfloat_function(Float32, m)
-    test_single_fastfloat_function(Float64, m)
 end
